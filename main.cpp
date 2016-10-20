951fb0f11876ddf6e818a3b6af1a4a3116c46475
angelineindahsi <angelineindahsi@ymail.com>
2016-10-20 23:17:16 +0700

    Updated

--- main.cpp
+++ main.cpp
@@ -10,14 +10,25 @@
 using namespace std;
 
 int main () {
+    int chancesLeft;
     char letter;
     string word;
     string wrongWord;
     string underscore;
     int numberOfWrong;
+    string words[] =
+    {
+        "Alpaca", "Butterfly", "Crocodile", "Crow", "Gazelle", "Hamster", "Lizard", "Hippopotamus", "Kangaroo", "Leopard", "Mongoose"
+    };
+    int n=rand()%10;
+    
+    cout << "Welcome to Hangman! Now try to guess a name of an animal!" << endl;
+    cin >> letter;
+    
+    
     
     for (int i=0; i!=word.length(); i++) {
-        if (word.at(i) == '') {
+        if (word.at(i) == ' ') {
             underscore += "_";
         } else {
             underscore += "_";
@@ -29,11 +40,13 @@ int main () {
     }
     
     if (numberOfWrong == 6) {
-        cout << "Sorry, you lose! The word is " << word << endl;
+        numberOfWrong = 6 - chancesLeft;
+        cout << "Sorry, you lose! The word is " << words << endl;
     }
     
-    cout << "This word has " << word.length() << "letters." << endl;
-    cout << "You have " << 6 - wrongWord << "tries left." << endl;
+    
+    
+    cout << "You have " << chancesLeft << " tries left." << endl;
     
     if (underscore == word) {
         cout << "Congratulations! You are correct!" << endl;
