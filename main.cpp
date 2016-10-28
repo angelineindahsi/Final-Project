a03f3c0996c58a8ba17af5b8cd51162cb769dab3
angelineindahsi <angelineindahsi@ymail.com>
2016-10-28 10:17:01 +0700

    Updated

--- main.cpp
+++ main.cpp
@@ -4,54 +4,60 @@
 //  Created by Angeline Indahsi on 10/13/16.
 //  Copyright © 2016 Angeline Indahsi. All rights reserved.
 
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main () {
-    int chancesLeft;
+const int maxTries = 6;
+int fill (char, string, string&);
+
+int main ()
+{
     char letter;
-    string word;
+    string correctWord;
     string wrongWord;
-    string underscore;
-    int numberOfWrong;
+    int numberOfWrong = 0;
     string words[] =
     {
-        "Alpaca", "Butterfly", "Crocodile", "Crow", "Gazelle", "Hamster", "Lizard", "Hippopotamus", "Kangaroo", "Leopard", "Mongoose"
+        "Alpaca", "Butterfly", "Crocodile", "Crow", "Gazelle", "Hamster", "Lizard", "Hippopotamus", "Kangaroo", "Leopard", "Mongoose", "Tiger", "Lion", "Cheetah", "Panther", "Mouse", "Armadillo", "Rhinoceros", "Giraffe", "Sloth", "Otter", "Hyena"
     };
-    int n=rand()%10;
+    int n=rand()%10; //The words appear will be random
+    correctWord=words[n];
+    string unknownWord(correctWord.length(), '_' ); //To replace the unkown words with _
     
     cout << "Welcome to Hangman! Now try to guess a name of an animal!" << endl;
     cin >> letter;
     
-    
-    
-    for (int i=0; i!=word.length(); i++) {
-        if (word.at(i) == ' ') {
-            underscore += "_";
-        } else {
-            underscore += "_";
-        }
+    while(numberOfWrong<maxTries)
+    {
+        cout << unknownWord;
+        cout << "Guess the letter: " << endl;
+        cin >> letter;
     }
     
-    for (int i=0; i!=50; ++i) {
-        cout << endl;
+    if (fill(letter, correctWord, unknownWord)==0)
+    {
+        cout << "Whoops! Wrong letter!" << endl;
+        numberOfWrong++;
+    }
+    else
+    {
+        cout << endl << "Yay! You got the correct letter! Keep it up!" << endl;
     }
     
-    if (numberOfWrong == 6) {
-        numberOfWrong = 6 - chancesLeft;
-        cout << "Sorry, you lose! The word is " << words << endl;
+    cout << "You have " << maxTries - numberOfWrong << " guessess left!" << endl;
+    
+    if (correctWord == unknownWord)
+    {
+        cout << correctWord << endl;
     }
     
-    
-    
-    cout << "You have " << chancesLeft << " tries left." << endl;
-    
-    if (underscore == word) {
-        cout << "Congratulations! You are correct!" << endl;
+    if (numberOfWrong == maxTries)
+    {
+        cout << "You have been hanged!" << endl;
+        cout << "Sorry you lose!" << endl;
+        cout << "The word is: " << correctWord << endl;
     }
-    
     return 0;
 }
 
